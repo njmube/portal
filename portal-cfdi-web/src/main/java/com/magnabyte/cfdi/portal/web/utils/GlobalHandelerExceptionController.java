eb9e56fc7f55dc8a1a261168260df04f013dc3d0
Edgar <edgarper1903@gmail.com>
2013-12-20 15:01:21 -0600

    Merge jacques4

--- portal-cfdi-web/src/main/java/com/magnabyte/cfdi/portal/web/utils/GlobalHandelerExceptionController.java
+++ portal-cfdi-web/src/main/java/com/magnabyte/cfdi/portal/web/utils/GlobalHandelerExceptionController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
-@ControllerAdvice
+//@ControllerAdvice
 public class GlobalHandelerExceptionController {
 
-	@ExceptionHandler(Exception.class)
+//	@ExceptionHandler(Exception.class)
 	public ModelAndView handleAllExceptions(Exception ex) {
 		ModelAndView model = new ModelAndView("error/genericError");
 		model.addObject("errMsg", "this is Exception.class");
