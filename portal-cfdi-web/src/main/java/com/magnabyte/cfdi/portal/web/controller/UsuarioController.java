a910cc8838cbafdaeb2523a35137ee2491757ecb
Clemente <sarcle_9212@hotmail.com>
2014-01-14 17:00:27 -0600

    catalogo usuarios

--- portal-cfdi-web/src/main/java/com/magnabyte/cfdi/portal/web/controller/UsuarioController.java
+++ portal-cfdi-web/src/main/java/com/magnabyte/cfdi/portal/web/controller/UsuarioController.java
@@ -1,15 +1,17 @@
 package com.magnabyte.cfdi.portal.web.controller;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.magnabyte.cfdi.portal.model.commons.Usuario;
+import com.magnabyte.cfdi.portal.service.commons.UsuarioService;
 
 @Controller
 public class UsuarioController {
@@ -17,11 +19,14 @@ public class UsuarioController {
 	private static final Logger logger = LoggerFactory
 			.getLogger(UsuarioController.class);
 	
+	@Autowired
+	private UsuarioService usuarioService;
+	
 	@RequestMapping("/catalogoUsuarios")
 	public String catalogoEstablecimiento(ModelMap model) {
 		logger.debug("-- catalogoUsuarios");
-		List<Usuario> usuarios = null;//establecimientoService.readAll();
-		model.put("usuarios", usuarios);
+//		List<Usuario> usuarios = usuarioService.getUsuarioByEstablecimiento(usuario)
+//		model.put("listaUsuarios", usuarios);
 		return "admin/listaEstablecimientos";
 	}
 	
@@ -35,6 +40,17 @@ public class UsuarioController {
 //		model.put("usuario", usuario);
 		
 		return "admin/usuarioForm";	
+	}
+	
+	@RequestMapping(value = "/guardarUsuario", method = RequestMethod.POST)
+	public String guardarUsuarios (@ModelAttribute Usuario usuario ) {
+		if (usuario.getId() != 0){
+//			usuarioService.update(usuario);
+		} else {
+//			usuarioService.save(usuario);
+		}
+		
+		return "redirect:/catalogoUsuarios";
 	}
 	
 }
